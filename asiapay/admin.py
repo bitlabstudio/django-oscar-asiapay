3e18ecdea535e7e2c01a97a88b1d97f20f019e7e
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2014-08-12 21:25:50 +0200

    Removed readonly fields from admin

--- asiapay/admin.py
+++ asiapay/admin.py
@@ -13,20 +13,6 @@ class AsiaPayTransactionAdmin(admin.ModelAdmin):
         'amount',
         'transaction_time',
     ]
-    readonly_fields = [
-        'payment_method',
-        'auth_id',
-        'pan_last_4',
-        'ip_country',
-        'account_holder',
-        'transaction_time',
-        'eci',
-        'pan_first_6',
-        'exp_month',
-        'exp_year',
-        'source_ip',
-        'pan_first_4',
-    ]
 
 
 admin.site.register(models.AsiaPayTransaction, AsiaPayTransactionAdmin)
